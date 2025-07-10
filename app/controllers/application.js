3803840a0e9a32aeec2eb91c1b8b636a234b271c
Reggie Bigornia <reggietheroman@gmail.com>
2025-07-10 13:30:30 +0800

    [Fix][Reggie] Add missing imports.

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -1,4 +1,6 @@
 import Controller from '@ember/controller';
+import { service } from '@ember/service';
+import { action } from '@ember/object';
 
 export default class ApplicationController extends Controller {
   @service session;
